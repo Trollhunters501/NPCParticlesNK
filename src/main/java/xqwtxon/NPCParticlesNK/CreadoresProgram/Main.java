eb83a66ec739734af0900f1e80fe19f0dbfa24f7
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-07-06 22:14:43 -0600

    Update Main.java

--- src/main/java/xqwtxon/NPCParticlesNK/CreadoresProgram/Main.java
+++ src/main/java/xqwtxon/NPCParticlesNK/CreadoresProgram/Main.java
@@ -1,1 +1,4 @@
-
+package xqwtxon.NPCParticlesNK.CreadoresProgram;
+import cn.nukkit.plugin.PluginBase;
+import xqwtxon.NPCParticlesNK.CreadoresProgram.ParticleEffect;
+public class Main extends PluginBase{}
