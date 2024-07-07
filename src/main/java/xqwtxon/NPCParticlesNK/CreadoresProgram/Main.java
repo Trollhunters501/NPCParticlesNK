ed88be4190984d71722715c26ef56aa5e1ef7943
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-07-06 22:49:57 -0600

    Update Main.java

--- src/main/java/xqwtxon/NPCParticlesNK/CreadoresProgram/Main.java
+++ src/main/java/xqwtxon/NPCParticlesNK/CreadoresProgram/Main.java
@@ -1,4 +1,26 @@
 package xqwtxon.NPCParticlesNK.CreadoresProgram;
 import cn.nukkit.plugin.PluginBase;
 import xqwtxon.NPCParticlesNK.CreadoresProgram.ParticleEffect;
-public class Main extends PluginBase{}
+import java.io.File;
+public class Main extends PluginBase{
+  @Override
+  public void onLoad(){
+    this.saveResource("config.yml");
+    if(this.getConfig().get("config-version") == "1.0.1"){
+      File confold = new File(this.getDataFolder()+"/config.yml");
+      confold.renameTo(new File(this.getDataFolder()+"/old-config.yml"));
+      this.getLogger().notice("Your configuration is outdated! The configuration was renamed as old-config.yml");
+      this.saveResource("config.yml");
+    }else{
+      this.saveDefaultConfig();
+    }
+  }
+  @Override
+  public void onEnable(){
+    int colorA = this.getConfig().getInt("Color A");
+    int colorB = this.getConfig().getInt("Color B");
+    int colorC = this.getConfig().getInt("Color C");
+    this.saveDefaultConfig();
+    this.getServer().getScheduler().scheduleRepeatingTask(new ParticleEffect(this), 2);
+  }
+}
